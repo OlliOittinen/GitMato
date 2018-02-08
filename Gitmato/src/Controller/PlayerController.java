d6dd047759cc3687b3f6eebe3007ea71a2a495e9
maxki <maxki@LAPTOP-ND1BDI3N.wlan.metropolia.fi>
2018-02-08 10:54:46 +0200

    Merge origin/master
    Conflicts:
    	Gitmato/src/Model/Board.java

--- Gitmato/src/Controller/PlayerController.java
+++ Gitmato/src/Controller/PlayerController.java
@@ -41,7 +41,7 @@ public class PlayerController  {
         // controlls for player 1      
         if (key == KeyEvent.VK_LEFT) {
             if(worms.get(0).getSuunta() != 2){
-                if(worms.get(0).getSuunta() != 1){
+                if(worms.get(0).getSuunta() != 1 && (worms.get(0).getX() < 20 || worms.get(0).getX() > 740)){
                     worms.get(0).setX(worms.get(0).getX()-9);
                 }
                 worms.get(0).setSuunta(1);
@@ -53,7 +53,7 @@ public class PlayerController  {
         if (key == KeyEvent.VK_RIGHT) {
             
             if(worms.get(0).getSuunta() != 1){
-                if(worms.get(0).getSuunta() != 2){
+                if(worms.get(0).getSuunta() != 2 && (worms.get(0).getX() < 20 || worms.get(0).getX() > 740)){
                     worms.get(0).setX(worms.get(0).getX()+9);
                 }
                 worms.get(0).setSuunta(2) ;
@@ -64,7 +64,7 @@ public class PlayerController  {
 
         if (key == KeyEvent.VK_UP) {
             if(worms.get(0).getSuunta() != 4){
-                if(worms.get(0).getSuunta() != 3){
+                if(worms.get(0).getSuunta() != 3 && (worms.get(0).getY() < 20 || worms.get(0).getY() > 530)){
                     worms.get(0).setY(worms.get(0).getY()-9);
                 }
                 worms.get(0).setSuunta(3);
@@ -74,7 +74,7 @@ public class PlayerController  {
 
         if (key == KeyEvent.VK_DOWN) {
             if(worms.get(0).getSuunta() != 3){
-               if(worms.get(0).getSuunta() != 4){
+               if(worms.get(0).getSuunta() != 4 && (worms.get(0).getY() < 20 || worms.get(0).getY() > 530)){
                     worms.get(0).setY(worms.get(0).getY()+9);
                 }
                worms.get(0).setSuunta(4);
@@ -86,7 +86,7 @@ public class PlayerController  {
         // controlls for player 2 
         if (key == KeyEvent.VK_A) {
             if(worms.get(1).getSuunta() != 2){
-                if(worms.get(1).getSuunta() != 1){
+                if(worms.get(1).getSuunta() != 1 && (worms.get(1).getX() < 20 || worms.get(1).getX() > 740)){
                     worms.get(1).setX(worms.get(1).getX()-9);
                 }
                 worms.get(1).setSuunta(1);
@@ -97,7 +97,7 @@ public class PlayerController  {
 
         if (key == KeyEvent.VK_D) {
             if(worms.get(1).getSuunta() != 1){
-                if(worms.get(1).getSuunta() != 2){
+                if(worms.get(1).getSuunta() != 2 && (worms.get(1).getX() < 20 || worms.get(1).getX() > 740)){
                     worms.get(1).setX(worms.get(1).getX()+9);
                 }
                 worms.get(1).setSuunta(2) ;
@@ -108,7 +108,7 @@ public class PlayerController  {
 
         if (key == KeyEvent.VK_W) {
             if(worms.get(1).getSuunta() != 4){
-                if(worms.get(1).getSuunta() != 3){
+                if(worms.get(1).getSuunta() != 3 && (worms.get(1).getY() < 20 || worms.get(1).getY() > 530)){
                     worms.get(1).setY(worms.get(1).getY()-9);
                 }
                 
@@ -119,7 +119,7 @@ public class PlayerController  {
 
         if (key == KeyEvent.VK_S) {
             if(worms.get(1).getSuunta() != 3){
-                if(worms.get(1).getSuunta() != 4){
+                if(worms.get(1).getSuunta() != 4 && (worms.get(1).getY() < 20 || worms.get(1).getY() > 530)){
                     worms.get(1).setY(worms.get(1).getY()+9);
                 }
                 
