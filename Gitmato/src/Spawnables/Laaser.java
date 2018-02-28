6022a91a86903f619df3d5ac012a3341162665c7
eeroni123 <eero.nieminen123@gmail.com>
2018-02-28 14:59:23 +0200

    working laser kinda

--- Gitmato/src/Spawnables/Laaser.java
+++ Gitmato/src/Spawnables/Laaser.java
@@ -11,11 +11,12 @@ import java.awt.Rectangle;
 import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.ImageIcon;
+
 /**
  *
  * @author Olli
  */
-public class Laaser implements Spawnables{
+public class Laaser implements Spawnables {
 
     private int xe;
     private int ye;
@@ -28,14 +29,13 @@ public class Laaser implements Spawnables{
     private Image image3;
     private boolean lethal = false;
     private Rectangle beam;
+    private boolean horizontal;
 
-    
+
     public Laaser() {
         init();
     }
-    
 
-    
     @Override
     public void loadImage(String imageName) {
         ImageIcon ii = new ImageIcon(imageName);
@@ -43,11 +43,9 @@ public class Laaser implements Spawnables{
     }
 
     public void damage(Worm worm) {
-        if (lethal) {
             Life.loseLife(worm);
-        }
     }
-    
+
     @Override
     public void init() {
         ImageIcon kuva = new ImageIcon("src/Images/Bombs(800-600).png");
@@ -56,7 +54,7 @@ public class Laaser implements Spawnables{
         image2 = kuva2.getImage();
         ImageIcon kuva3 = new ImageIcon("src/Images/IronVertical.png");
         image3 = kuva3.getImage();
-            
+
         setX(-100);
         setY(-100);
         setX2(-1000);
@@ -65,88 +63,81 @@ public class Laaser implements Spawnables{
         setY3(-1000);
     }
 
-    
     public void onPickup(Worm worm, Worm worm2) {
-        worm.setPoints(worm.getPoints()+100);
-        
+        worm.setPoints(worm.getPoints() + 100);
+
         //hae kohteen nykysijainti, tallenna muuttujiin
         int wormLocX = worm2.getX();
         int wormLocY = worm2.getY();
-        
+
         //horisontaalinen vai vertikaalinen säde, random arvo 0...1
         double r = Math.random();
         //hae luotavan säteen rajat, laita sijainti 2. madon nykypaikkaan
-        if (r<0.5) {
-            beam = getBoundsHorizontal();
-            beam.setFrameFromCenter(wormLocX, wormLocY, (wormLocX+800)/2, 0);
-        }
-        else {
-            beam = getBoundsVertical();
-            beam.setFrameFromCenter(wormLocX, wormLocY, 0, (wormLocY+600)/2);
-        }   
-        //ajastin säteelle, jonka jälkeen säteestä tappava
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                lethal = true;
-                }
-            }, 2000);
-        
+        if (r < 0.5) {
+            horizontal = false;
+            randomizeXY2();
+            
+        } else {
+            horizontal = true;
+            randomizeXY2();
+
     }
+    }
+
     //ikoni powerupille
     @Override
     public Rectangle getBounds() {
-        return new Rectangle(xe+3, ye+3, 30, 30);
+        return new Rectangle(xe + 3, ye + 3, 30, 30);
     }
-    
-    public Rectangle getBoundsVertical() {
-        return new Rectangle(xe+3, ye+3, 800, 100);
+
+    public Rectangle getBoundsB() {
+        if (horizontal == true) {
+            return new Rectangle(xe3 + 3, ye3 + 3, 100, 600);
+        } else {
+            return new Rectangle(xe2 + 3, ye2 + 3, 800, 100);
+        }
     }
-    
-    public Rectangle getBoundsHorizontal() {
-        return new Rectangle (xe+3, ye+3, 100, 600);
-    }
-    
+
+
     public Rectangle getBeam() {
         return beam;
     }
 
     @Override
     public int getX() {
-        return xe;    
+        return xe;
     }
 
     @Override
     public int getY() {
         return ye;
     }
-    
-    public int getXe2() {
+
+    public int getX2() {
         return xe2;
     }
 
-    public int getYe2() {
+    public int getY2() {
         return ye2;
     }
 
-    public int getXe3() {
+    public int getX3() {
         return xe3;
     }
 
-    public int getYe3() {
+    public int getY3() {
         return ye3;
     }
-    
+
     @Override
     public void setX(int x) {
         this.xe = x;
     }
-    
+
     public void setX2(int x) {
         this.xe2 = x;
     }
-    
+
     public void setX3(int x) {
         this.xe3 = x;
     }
@@ -155,30 +146,42 @@ public class Laaser implements Spawnables{
     public void setY(int y) {
         this.ye = y;
     }
-    
+
     public void setY2(int y) {
         this.ye2 = y;
     }
-    
+
     public void setY3(int y) {
         this.ye3 = y;
     }
+
     @Override
     public Image getImage() {
         return image;
     }
-    
+
     public Image getImageHori() {
         return image2;
     }
-    
+
     public Image getImageVert() {
         return image3;
     }
+
     @Override
     public void randomizeXY() {
         setX((int) (Math.random() * 750));
         setY((int) (Math.random() * 550));
+    }
+
+    public void randomizeXY2() {
+        setX2((int) (Math.random() * 750));
+        setY2(0);
+    }
+
+    public void randomizeXY3() {
+        setX3(0);
+        setY3((int) (Math.random() * 550));
     }
 
 }
